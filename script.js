138202337f776f67412eab2b65b15fd80eef528b
Hubert Muller <hubert@sportport.eu>
2021-09-04 14:04:51 +0200

    zasiegi

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 var elementyH2 = document.querySelectorAll("h2");
 elementyH2[0].innerText = "Cześć";
+var b = 10;
 
 function zmien() {
+    //var b = 5;
     elementyH2[0].innerText = "Własnie się zmieniłem";
+    b = 7;
 };
+
+function a() {
+    //var b = 5;
+    var b = 6;
+    b = b* b;
+    console.log('w funkcji a, b ma wartosc:' + b);
+    console.log(document);
+}
+
+function test() {
+    //let x = 5;
+    for(let x = 0; x<100; x++) {
+        console.log(x);
+    };
+    console.log(x);
+}
+
+test();
+console.log(x);
+
+if (x == 5)
+    console.log('hej');
+
+if (x == 6)
+    console.log('hej');
+
+
+/*
+a();
+console.log(b);
+ */
+
+
 
