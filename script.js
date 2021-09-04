33f343ac5dd35ba0083f39ca3be6c2d4a23966df
Hubert Muller <hubert@sportport.eu>
2021-09-04 14:48:28 +0200

    formularz

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-var elementyH2 = document.querySelectorAll("h2");
-elementyH2[0].innerText = "Cześć";
-var b = 10;
+var formularz = document.forma;
+//var formularz = document.querySelector('form[name=forma]');
+formularz.addEventListener('submit', (cel) => {
 
-function zmien() {
-    //var b = 5;
-    elementyH2[0].innerText = "Własnie się zmieniłem";
-    b = 7;
-};
+    let blad = false;
+    let bledy = '';
 
-function a() {
-    //var b = 5;
-    var b = 6;
-    b = b* b;
-    console.log('w funkcji a, b ma wartosc:' + b);
-    console.log(document);
+    if (!formularz.imie.value.match(/^([A-z]{3,})$/g)) {
+        bledy = '<p class="blad">imie jest zle<p>';
+        blad = true;
+    }
+
+    if (!formularz.nazwisko.value.match(/^([A-z]{2,})(\-){0,1}([A-z]{2,})$/g)) {
+        bledy += '<p class="blad">nazwisko jest zle<p>';
+        blad = true;
+    }
+
+    if (blad == true) {
+        cel.preventDefault();
+        pokazBlad('Wystapil blad', bledy);
+    }
+    //if (blad) ....
+})
+
+function pokazBlad(tytul, tekst) {
+    var tytulBledu = document.createElement('h3');
+    var tekstBledu = document.createElement('div');
+    var divBlad = document.querySelector('#blad');
+    tytulBledu.innerText = tytul;
+    tekstBledu.innerHTML = tekst;
+    formularz.insertBefore(tytulBledu, divBlad);
+    formularz.insertBefore(tekstBledu, divBlad);
 }
-
-function test() {
-    //let x = 5;
-    for(let x = 0; x<100; x++) {
-        console.log(x);
-    };
-    console.log(x);
-}
-
-test();
-console.log(x);
-
-if (x == 5)
-    console.log('hej');
-
-if (x == 6)
-    console.log('hej');
-
-
-/*
-a();
-console.log(b);
- */
-
-
-
